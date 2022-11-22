183737f5a9051ba871811a9c4465e425ba35523e
Rushil Punyamurthula <rushil.punya1@gmail.com>
2022-11-22 11:55:44 -0700

    Finished query functionality
    Signed-off-by: Rushil Punyamurthula <rushil.punya1@gmail.com>

--- client/src/components/ListInfo.js
+++ client/src/components/ListInfo.js
@@ -1,12 +1,38 @@
 import React,{Fragment, useEffect, useState} from "react";
 
 const ListInfo = () => {
-    const [info, setInfo] = useState([]);
+    const [constructor, setConstructor] = useState([]);
+    const [driver, setDriver] = useState([]);
+    const [race, setRace] = useState([]);
     const [year, setYear] = useState("");
     const getInfo = async e => {
         e.preventDefault();
         try {
             var response;
+            if(year != null)
+            {
+                response = await fetch("http://localhost:5000/constructors/" + year);
+            }
+            else
+            {
+                response = await fetch("http://localhost:5000/constructors/");
+            }
+            
+            var jsonData = await response.json();
+            setConstructor(jsonData);
+
+            if(year != null)
+            {
+                response = await fetch("http://localhost:5000/drivers/" + year);
+            }
+            else
+            {
+                response = await fetch("http://localhost:5000/drivers/");
+            }
+            
+            jsonData = await response.json();
+            setDriver(jsonData);    
+
             if(year != null)
             {
                 response = await fetch("http://localhost:5000/races/" + year);
@@ -16,8 +42,8 @@ const ListInfo = () => {
                 response = await fetch("http://localhost:5000/races/");
             }
             
-            const jsonData = await response.json();
-            setInfo(jsonData);  
+            jsonData = await response.json();
+            setRace(jsonData);    
         } catch (error) {
             console.error(error.message);
         }
@@ -33,22 +59,85 @@ const ListInfo = () => {
             <input type = "text" id = "year" className = "form-control mr-3" value = {year} onChange={e => setYear(e.target.value)}/>
             <button className = "btn btn-success">Search</button>
         </form>
-        <table class="table mt-5 text-center">
+        <table class="table mt-5 mr-5 text-center">
         <thead>
             <tr>
-            <th scope="col">Description</th>
+                <th scope="col">Constructors</th>
+                <th scope="col">Drivers</th>
+                <th scope="col">Races</th>
             </tr>
         </thead>
         <tbody>
-            {/*<th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>*/}
-            {info.map(entry => (
+        <tr>
+        <td>
+            <table class="table mt-5 text-center">
+            <thead>
                 <tr>
-                    <td>{entry.year}</td>
+                    <th scope="col">Position</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Points</th>
+                    <th scope="col">Url</th>
+                </tr>
+            </thead>
+            <tbody>
+                {constructor.map(entry => (
+                    <tr>
+                        <td>{entry.position}</td>
+                        <td>{entry.name}</td>
+                        <td>{entry.points}</td>
+                        <td>{entry.url}</td>
+                    </tr>
+                ))}
+            </tbody>
+            </table>
+        </td>
+        <td>
+        <table class="table mt-5 text-center">
+        <thead>
+            <tr>
+                <th scope="col">Position</th>
+                <th scope="col">Forename</th>
+                <th scope="col">Surname</th>
+                <th scope="col">Points</th>
+                <th scope="col">Url</th>
+            </tr>
+        </thead>
+        <tbody>
+            {driver.map(entry => (
+                <tr>
+                    <td>{entry.position}</td>
+                    <td>{entry.forename}</td>
+                    <td>{entry.surname}</td>
+                    <td>{entry.points}</td>
+                    <td>{entry.url}</td>
                 </tr>
             ))}
+        </tbody>
+        </table>
+        </td>
+        <td>
+        <table class="table mt-5 text-center">
+        <thead>
+            <tr>
+                <th scope="col">Circuit</th>
+                <th scope="col">Date</th>
+                <th scope="col">Round</th>
+                <th scope="col">Url</th>
+            </tr>
+        </thead>
+        <tbody>
+            {race.map(entry => (
+                <tr>
+                    <td>{entry.name}</td>
+                    <td>{entry.date}</td>
+                    <td>{entry.round}</td>
+                    <td>{entry.url}</td>
+                </tr>
+            ))}
+        </tbody>
+        </table>
+        </td>
+        </tr>
         </tbody>
         </table>
     </Fragment>
--- server/index.js
+++ server/index.js
@@ -6,7 +6,49 @@ const pool = require("./db")
 app.use(cors());
 app.use(express.json());
 
-//test to get all races
+//get all constructors
+app.get("/constructors", async(req, res) => {
+    try {
+        const constructors = await pool.query("SELECT constructorstandings.position, constructors.name, constructorstandings.points, constructors.url FROM constructors JOIN constructorstandings");
+        res.json(constructors.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+//get constructor of specific year
+app.get("/constructors/:year", async(req, res) => {
+    try {
+        const {year} = req.params;
+        const constructors = await pool.query("SELECT constructorstandings.position, constructors.name, constructorstandings.points, constructors.url FROM constructors JOIN constructorstandings ON constructors.constructorid = constructorstandings.constructorid WHERE raceid = (SELECT raceid FROM races WHERE round = ( SELECT MAX(round) as lastrace FROM races WHERE year = $1) AND year = $1) ORDER BY position ASC", [year]);
+        res.json(constructors.rows)
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+//get all drivers
+app.get("/drivers", async(req, res) => {
+    try {
+        const drivers = await pool.query("SELECT driverstandings.position, drivers.forename, drivers.surname, driverstandings.points, drivers.url FROM drivers JOIN driverstandings");
+        res.json(drivers.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+//get driver of specific year
+app.get("/drivers/:year", async(req, res) => {
+    try {
+        const {year} = req.params;
+        const drivers = await pool.query("SELECT driverstandings.position, drivers.forename, drivers.surname, driverstandings.points, drivers.url FROM drivers JOIN driverstandings ON drivers.driverid = driverstandings.driverid WHERE raceid = (SELECT raceid FROM races WHERE round = ( SELECT MAX(round) as lastrace FROM races WHERE year = $1) AND year = $1) ORDER BY position ASC", [year]);
+        res.json(drivers.rows)
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+//get all races
 app.get("/races", async(req, res) => {
     try {
         const races = await pool.query("SELECT * FROM races");
@@ -16,11 +58,11 @@ app.get("/races", async(req, res) => {
     }
 })
 
-//get specific year
+//get race of specific year
 app.get("/races/:year", async(req, res) => {
     try {
         const {year} = req.params;
-        const races = await pool.query("SELECT * FROM races WHERE year = $1", [year])
+        const races = await pool.query("SELECT circuits.name, races.date, races.round, races.url FROM races JOIN circuits ON races.circuitid = circuits.circuitid WHERE year = $1", [year]);
         res.json(races.rows)
     } catch (err) {
         console.error(err.message);
